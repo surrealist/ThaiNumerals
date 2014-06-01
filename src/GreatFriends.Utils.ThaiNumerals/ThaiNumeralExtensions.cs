f369c0b9aa6058509f77f66941bfbddaee5cd87e
Suthep Saengwirotjanapat <suthep@gfbd.co.th>
2014-06-01 17:43:45 +0700

    initial

---
+++ src/GreatFriends.Utils.ThaiNumerals/ThaiNumeralExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreatFriends.Utils {
+
+  public static class ThaiNumeralExtensions {
+
+    public static string ToThaiNumerals(this int value) {
+      return internalToThaiNumerals(value.ToString());
+    }
+    public static string ToThaiNumerals(this long value) {
+      return internalToThaiNumerals(value.ToString());
+    }
+    public static string ToThaiNumerals(this float value) {
+      return internalToThaiNumerals(value.ToString());
+    }
+    public static string ToThaiNumerals(this double value) {
+      return internalToThaiNumerals(value.ToString());
+    }
+    public static string ToThaiNumerals(this decimal value) {
+      return internalToThaiNumerals(value.ToString());
+    }
+
+    private static string internalToThaiNumerals(string text) {
+      const int diff = (int)'๑' - (int)'1';
+      var sb = new StringBuilder();
+      foreach (char ch in text) {
+        if ('0' <= ch && ch <= '9')
+          sb.Append((char)((int)ch + diff));
+        else
+          sb.Append(ch);
+      }
+      return sb.ToString();
+    }
+  }
+}
---
+++ src/GreatFriends.Utils.ThaiNumeralsTests/ThaiNumeralExtensionsTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GreatFriends.Utils;
+
+namespace GreatFriends.Utils.ThaiNumeralsTests {
+
+  public class ThaiNumeralExtensionsTests {
+
+    /// <summary>
+    /// แสดงข้อความค่าตัวเลขด้วยตัวอักษรเลขไทย 
+    /// เช่น ค่า 12345.67 เป็น "๑๒,๓๔๕.๖๗"
+    /// </summary>
+    [TestFixture]
+    public class TheToThaiNumeralsMethod {      
+      
+      [Test]
+      public void GeneralUsage_ShouldBeUsableWithNumericTypes() {
+        int valInt32       = 123;
+        long valInt64      = 123L;
+        float valSingle    = 123F;
+        double valDouble   = 123.0;
+        decimal valDecimal = 123M;
+        
+        string strInt32 = valInt32.ToThaiNumerals();
+        string strInt64 = valInt64.ToThaiNumerals();
+        string strSingle = valSingle.ToThaiNumerals();
+        string strDouble = valDouble.ToThaiNumerals();
+        string strDecimal = valDecimal.ToThaiNumerals();         
+      }
+
+      [Test]
+      public void ZeroToNine() {
+        double val = 1234567890.123;
+
+        string str = val.ToThaiNumerals();
+
+        Assert.AreEqual("๑๒๓๔๕๖๗๘๙๐.๑๒๓", str);
+      }
+    }
+  }
+}
